3714e08c137817db2940f3d8a22d6da88a0492d7
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-03-27 10:53:53 +0100

    Load the avatar into the layout

--- demos/kotlin-app/src/main/kotlin/com/pusher/chatkitdemo/images/ImageViewExtensions.kt
+++ demos/kotlin-app/src/main/kotlin/com/pusher/chatkitdemo/images/ImageViewExtensions.kt
@@ -2,11 +2,11 @@ package com.pusher.chatkitdemo.images
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.pusher.chatkit.R
+import com.pusher.chatkitdemo.R
 
 var ImageView.imageUrl : CharSequence
     get() = getTag(R.id.image_url) as String
     set(value) {
-        Glide.with(this).load(value)
+        Glide.with(this).load(value).into(this)
         setTag(R.id.image_url, value)
     }
